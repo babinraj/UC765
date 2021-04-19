42ce49dfc8ad3add007e9062b839fcaa83cb5bb9
School <school@yazataconsulting.com>
2021-04-19 07:08:54 +0530

    environment.prod.ts,index.html

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // baseUrl: 'http://139.99.88.196:8080/mdmscheldt/',
-  baseUrl: 'http://139.99.88.196:8080/mdmscheldt/'
+  baseUrl: '/mdmscheldt/'
 };
